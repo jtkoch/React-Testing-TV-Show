4a1362f9f21f9ae1d2b10885d8691cfe045fca7d
Jensen Koch <jtkoch12@gmail.com>
2020-03-11 20:59:58 -0500

    Stuck on my async testing in app.test.js

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -6,7 +6,12 @@ const fetchShow = () => {
     "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
   )
   .then(res => {
-    return res});
-};  
+    return res
+  })
+  .catch(err => {
+    console.error('error fetching data from api, err: ', err.message);
+    return err;
+  });  
+}
 
 export default fetchShow;
